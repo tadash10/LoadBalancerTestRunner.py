8a1b8a118df6753d5283a3f7631deecaf09141b4
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-16 23:14:07 -0300

    Create v2.py

---
+++ v2.py
@@ -0,0 +1,95 @@
+import sys
+import subprocess
+import os
+
+GATLING_HOME = "/path/to/gatling"  # Set the path to your Gatling installation
+
+def install_gatling():
+    # Check if Gatling is already installed
+    if not os.path.exists(GATLING_HOME):
+        print("Gatling not found. Installing...")
+        
+        # Add code here to install Gatling using the appropriate package manager or method
+        # Based on ISO standards, use standardized installation processes
+
+        print("Gatling installed successfully.")
+    else:
+        print("Gatling is already installed.")
+
+def setup_gatling():
+    # Set up Gatling configuration files, scenarios, and simulations
+    if os.path.exists(GATLING_HOME):
+        print("Setting up Gatling...")
+        
+        # Add code here to copy necessary configuration files, scenarios, and simulations
+        # Based on ISO standards, follow standardized file organization and naming conventions
+        
+        print("Gatling setup completed.")
+    else:
+        print("Gatling is not installed. Please install it first.")
+
+def run_load_test():
+    endpoint = input("Enter the load balancer endpoint or IP address: ")
+    num_users = input("Enter the number of virtual users to simulate: ")
+    
+    # Execute Gatling command to run the load test
+    subprocess.run([
+        "sh", 
+        f"{GATLING_HOME}/bin/gatling.sh", 
+        "-s", "your_simulation_file", 
+        "-u", num_users, 
+        "-e", 
+        "-o", "results"
+    ], check=True)
+
+def generate_report():
+    # Generate and display the load test report
+    report_path = "results"  # Path to the Gatling report directory
+    
+    if os.path.exists(report_path):
+        print("Generating load test report...")
+        
+        # Add code here to process the report and display or save the results
+        # Based on ISO standards, ensure report generation follows standardized formats
+        
+        print("Load test report generated.")
+    else:
+        print("Load test report not found. Please run the load test first.")
+
+def cleanup():
+    # Clean up any temporary or unnecessary files
+    # Add code here to delete any temporary files or directories created during the load test
+
+    print("Cleanup completed.")
+
+def print_menu():
+    print("==== Load Balancer Test Menu ====")
+    print("1. Run Load Test")
+    print("2. Generate Report")
+    print("3. Cleanup")
+    print("4. Exit")
+
+def menu_choice():
+    while True:
+        print_menu()
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            run_load_test()
+        elif choice == "2":
+            generate_report()
+        elif choice == "3":
+            cleanup()
+        elif choice == "4":
+            print("Exiting...")
+            sys.exit(0)
+        else:
+            print("Invalid choice. Please try again.")
+
+def main():
+    install_gatling()
+    setup_gatling()
+    menu_choice()
+
+if __name__ == "__main__":
+    main()
