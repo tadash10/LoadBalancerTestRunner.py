f3718167b799a2020560cf35dfe86c025b77c984
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-16 23:12:10 -0300

    Create cleanup.py

---
+++ cleanup.py
@@ -0,0 +1,7 @@
+import os
+
+def cleanup():
+    # Clean up any temporary or unnecessary files
+    # Add code here to delete any temporary files or directories created during the load test
+
+    print("Cleanup completed.")
